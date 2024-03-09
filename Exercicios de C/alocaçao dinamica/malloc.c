a5a8cfbbea2248416df5e50cff6e78f94d1503a8
Filipe Samuel <felipepires.p125@gmail.com>
2024-03-09 01:11:42 -0300

    Adicionando alocação dinamica

---
+++ Exercicios de C/alocaçao dinamica/malloc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+
+
+int main(void){
+   
+  int tamanho_do_vetor, *ptr;
+
+ printf("Digite o tamanho do vetor: ");
+ scanf("%d", &tamanho_do_vetor);
+ 
+  int byte_necessario = tamanho_do_vetor * sizeof(int); // mostrando que vai retornar um inteiro
+  ptr = malloc(byte_necessario); // vai retornar um endereço que tem uma memoria de tamanho "byte_tamanho" 
+                                 // de maneira sequencial, lembrando um vetor
+  
+  printf("\n\n"); 
+
+  for (int i = 0; i < tamanho_do_vetor; i++){
+    printf("Digite o %d valor do vetor: ", (i + 1));
+    scanf("%d", &ptr[i]);
+  }
+  printf("\n\n");
+
+  for( int i = 0; i < tamanho_do_vetor; i++){
+    printf("O %d valor do vetor é: %d\n", (i+1), ptr[i]);
+  }
+  free(ptr);
+  
+
+
+
+
+    return 0;
+}
