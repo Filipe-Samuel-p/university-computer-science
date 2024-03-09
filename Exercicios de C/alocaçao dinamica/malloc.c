6430513a3ddb1865a7d732e9caa172eeed749764
Filipe Samuel <felipepires.p125@gmail.com>
2024-03-09 15:59:09 -0300

    alocacao dinamica

--- Exercicios de C/alocaçao dinamica/malloc.c
+++ Exercicios de C/alocaçao dinamica/malloc.c
@@ -5,27 +5,27 @@
 
 int main(void){
    
-  int tamanho_do_vetor, *ptr;
+  int tamanho_do_vetor, *vet;
 
  printf("Digite o tamanho do vetor: ");
  scanf("%d", &tamanho_do_vetor);
  
   int byte_necessario = tamanho_do_vetor * sizeof(int); // mostrando que vai retornar um inteiro
-  ptr = malloc(byte_necessario); // vai retornar um endereço que tem uma memoria de tamanho "byte_tamanho" 
+  vet = malloc(byte_necessario); // vai retornar um endereço que tem uma memoria de tamanho "byte_tamanho" 
                                  // de maneira sequencial, lembrando um vetor
   
   printf("\n\n"); 
 
   for (int i = 0; i < tamanho_do_vetor; i++){
     printf("Digite o %d valor do vetor: ", (i + 1));
-    scanf("%d", &ptr[i]);
+    scanf("%d", &vet[i]);
   }
   printf("\n\n");
 
   for( int i = 0; i < tamanho_do_vetor; i++){
-    printf("O %d valor do vetor é: %d\n", (i+1), ptr[i]);
+    printf("O %d valor do vetor é: %d\n", (i+1), vet[i]);
   }
-  free(ptr); // liberando o espaço alocado (reservado)
+  free(vet); // liberando o espaço alocado (reservado)
   
 
 
