19a31a08e333a4c890890b48f4e638212f301537
Filipe Samuel <felipepires.p125@gmail.com>
2024-03-31 22:13:44 -0300

    TAD

--- data structures_1/TAD/ponto.c
+++ data structures_1/TAD/ponto.c
@@ -4,34 +4,36 @@
 #include <stdlib.h>
 
 struct ponto {
-  float ponto_x;
-  float ponto_y;
+    float ponto_x;
+    float ponto_y;
 };
 
 Pontos *pto_cria(float x, float y) {
-  Pontos *p;
-  p = (Pontos *)malloc(sizeof(Pontos));
-  if (p != NULL) {
-    p->ponto_x = x;
-    p->ponto_y = y;
-  }
-  return p;
+    Pontos *p;
+    p = (Pontos *)malloc(sizeof(Pontos));
+    if (p != NULL) {
+        p->ponto_x = x;
+        p->ponto_y = y;
+    }
+    return p;
 }
 
 void pto_libera(Pontos *p) { free(p); }
 
 void pto_acessa(Pontos *p, float *x, float *y) {
-  *x = p->ponto_x;
-  *y = p->ponto_y;
+    *x = p->ponto_x;
+    *y = p->ponto_y;
 }
 void pto_atribui(Pontos *p, float x, float y) {
-  p->ponto_x = x;
-  p->ponto_y = y;
+    p->ponto_x = x;
+    p->ponto_y = y;
 }
 
 float pto_distancia(Pontos *p1, Pontos *p2) {
-  float distancia;
-  distancia = sqrt(pow((p2->ponto_x - p1->ponto_x), 2)) +
-              pow((p2->ponto_y - p1->ponto_y), 2);
-  return distancia;
+    float distancia,dx,dy;
+    dx = p1->ponto_x - p2->ponto_x;
+    dy = p1->ponto_y - p2->ponto_y;
+    distancia = sqrt(dx * dx + dy * dy);
+
+    return distancia;
 }
