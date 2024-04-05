fabdeb1685c0137a9612ec9938dc4415894e5d55
Filipe Samuel <felipepires.p125@gmail.com>
2024-04-05 15:46:21 -0300

    selection sort

--- data structures_1/ordenaçao/selection_sort.c
+++ data structures_1/ordenaçao/selection_sort.c
@@ -2,57 +2,64 @@
 #include <stdlib.h>
 #include <time.h>
 
-void criacao_vet_aleat (int *vet, int taman){
-    srand(time(NULL));
-    for (int i = 0; i < taman; i++){
+void vetor_aleatorio(int *vet, int number){
+    srand(time(NULL)); 
+    for(int i = 0; i < number; i++){
         vet[i] = rand()%100;
     }
-}   
+}
 
-void mostra_Vetor_ale(int *vet,int taman){
-    for (int i = 0; i < taman; i++){
-        printf("****** vetor aleatorio ******\n\n");
-        printf("%d,\t", vet[i]);
+void mostrar_vet_alea(int *vet, int number){
+    
+    printf("******* VETOR ALEATORIO ********\n\n");
+    
+    for(int i = 0; i<number;i++){
+        printf("%d\t", vet[i]);
+    }
+    printf("\n\n");
+}
+
+void selection_sort(int *vet,int number){ // tem que alterar algo aqui!!!
+    int x = 0,aux;
+    while (x < number){
+        for(int i=0; i<number; i++){
+            if (vet[x]>vet[x+i]){
+                aux = vet[i];
+                vet[i] = vet[x];
+                vet[x] = aux;
+            }
+        }
+        x += 1;
     }
 }
 
-void selection_sort(int *vet,int taman){
-  int x = 0, aux;
-  while (x <= taman){
-    for (int i = 0; i <  taman; i++){
-        if (vet[x] > vet[i]){
-            aux = vet[x];
-            vet[i] = vet[i];
-            vet[i] = aux;
-         }
+void mostrar_vet_ordenado(int *vet, int number){
+   
+    printf("****** VETOR ORDENADO *******\n\n");
+   
+    for(int i = 0; i<number;i++){
+        printf("%d\t", vet[i]);
     }
-    x += 1;
-  }
+    printf("\n\n");
 }
 
-void mostra_Vetor_org(int *vet,int taman){
-    for (int i = 0; i < taman; i++){
-        printf("****** vetor ordenado ******\n\n");
-        printf("%d,\t", vet[i]);
-    }
-}
 
 
 int main(void){
-   int *tamanho_vetor,n;
-   printf("Digite o tamanho do vetor: ");
-   scanf("%d", &n);
+  int *tamanho_vetor,n;
 
-   tamanho_vetor = malloc(n * sizeof(int));
-   if(tamanho_vetor){
-        int vetor_aleatorio[n];
-        criacao_vet_aleat(vetor_aleatorio,n);
-        mostra_Vetor_ale(vetor_aleatorio,n);
-        selection_sort(vetor_aleatorio, n);
-        mostra_Vetor_org(vetor_aleatorio,n);
-   }
-   
-   free(tamanho_vetor);
+  printf("Digite o tamanho do vetor: ");
+  scanf("%d", &n);
+
+  tamanho_vetor = (int*)malloc(n * sizeof(int));
+  if (tamanho_vetor){
+     vetor_aleatorio(tamanho_vetor,n);
+     mostrar_vet_alea(tamanho_vetor,n);
+     selection_sort(tamanho_vetor,n);
+     mostrar_vet_ordenado(tamanho_vetor,n);
+  }
+
+
 
 
     return 0;
