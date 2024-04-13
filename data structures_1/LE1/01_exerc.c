553e78dd814f8b27db45d034fc3a9767be10a03c
Filipe Samuel <felipepires.p125@gmail.com>
2024-04-13 17:21:59 -0300

    Linked list

--- data structures_1/LE1/01_exerc.c
+++ data structures_1/LE1/01_exerc.c
@@ -10,42 +10,61 @@ void clearScreen() { // funcao para limpar o terminal
     system("clear"); 
 }
 
-void alocacao_vetor(AGENDA **agenda, int numero_de_elementos){
-    (*agenda) = (AGENDA *)realloc(*agenda,(numero_de_elementos + 1) * sizeof(AGENDA));
+void alocacao_vetor(AGENDA **agenda, int *numero_de_elementos){
+    (*agenda) = (AGENDA *)realloc(*agenda,(*numero_de_elementos + 1) * sizeof(AGENDA));
     if ((*agenda) == NULL){
       printf("ERRO DE ALOCACAO");
     }
 }
 
-void inclusao(AGENDA *agenda){
+void inclusao(AGENDA *agenda,int numero){
 
-   printf("\n**** INCLUSAO ******\n\n");
-   printf("Digite o nome: \n");
-   fgets(agenda->nome,100,stdin);
-   printf("\nDigite o email: ");
-   fgets(agenda->email,100,stdin);
-   printf("\nDigite o telefone: ");
-   fgets(agenda->telefone, 20,stdin);
+   printf("\n**** INCLUSAO ******\n");
+   printf("Digite o nome: ");
+   scanf("%s", agenda[numero].nome);
+   printf("Digite o email: ");
+   fgets(agenda[numero].email,100,stdin);
+   if (fgets(agenda[numero].email,100,stdin) == NULL){
+     printf("ERRO");
+   }
+   printf("Digite o telefone: ");
+   fgets(agenda[numero].telefone, 20,stdin);
 }
 
-void mostrar_agenda(AGENDA agenda, int tamanho){
+void consulta(AGENDA agenda, int tamanho){
    
    for (int i = 0; i < tamanho;i++){
-    printf("O %d elemento da agenda: \n\n", tamanho);
+    printf("\nO %d elemento da agenda: \n\n", tamanho);
     printf("Nome: %s\n", agenda.nome);
     printf("Email: %s\n", agenda.email);
     printf("telefone: %s\n", agenda.telefone);
    }
 }
 
-void alteracao(AGENDA *agenda,int tamanho){    // PAREI AQUIIIIIII
-   int escolha;
-   printf("Qual elemento quer alterar: ");
-   scanf("%d",&escolha);
-
-   if (escolha == 1){
-
-   }
+void alteracao(AGENDA *agenda,int tamanho){    
+   char op,nome[100];
+   int i;
+  
+   for(int i=0; i < tamanho && strncmp( agenda[i].nome,nome, strlen(nome))!=0;i++);
+      if( i>= tamanho){
+        printf("registro não encontrado");
+      }
+      else{
+         printf("\n\tRegistro %d", i);
+         printf("\n\tNome : %s", agenda[i].nome );
+         printf("\n\tEmail : %s", agenda[i].email );
+         printf("\n\tFone : %s", agenda[i].telefone );
+         printf("\n\tConirma a alteracao ?");
+         scanf("%c", &op);
+         if (op == "S"  || op == "s"){
+            printf("Digite o novo nome: ");
+            fgets(agenda[i].nome,100,stdin);
+            printf("\nDigite o novo email: ");
+            fgets(agenda[i].email,100,stdin);
+            printf("\nDigite o novo telefone: ");
+            fgets(agenda[i].telefone,20,stdin);
+         }
+      }
 }
 
 
@@ -57,7 +76,7 @@ int main(void){
  
  do {
 
-      printf("***** ESCOLHA UMA DAS OPCOES ABAIXO: \n\n");
+      printf("\n***** ESCOLHA UMA DAS OPCOES ABAIXO: \n\n");
       printf("1- inclusao \n");
       printf("2- Alteracao \n");
       printf("3- Consulta \n");
@@ -71,22 +90,22 @@ int main(void){
       switch (opcao)
       {
       case 1:
-         alocacao_vetor(&ptrAgenda,numero_de_elementos);
-         inclusao(&ptrAgenda[numero_de_elementos]);
+         alocacao_vetor(&ptrAgenda,&numero_de_elementos);
+         inclusao(ptrAgenda,numero_de_elementos);
          numero_de_elementos++;
          break;
       case 2:
          clearScreen();
-         mostrar_agenda(*ptrAgenda,numero_de_elementos);
+         alteracao(ptrAgenda,numero_de_elementos);
          break;
       case 3:
-         /* code */
+         consulta(*ptrAgenda,numero_de_elementos);
          break;
       case 4:
          /* code */
          break;
       case 5:
-         /* code */
+         consulta(*ptrAgenda, numero_de_elementos);
          break;
       case 6:
          /* code */
