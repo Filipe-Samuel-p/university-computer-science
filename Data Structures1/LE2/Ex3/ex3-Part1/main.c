19907a27e493cfb2d5859ec68cf8998b955f5ddd
Filipe Samuel <felipepires.p125@gmail.com>
2024-05-28 18:56:53 -0300

    equeue

---
+++ Data Structures1/LE2/Ex3/ex3-Part1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAMANHOFILA 10
+
+typedef struct fila{
+    int head, tail;
+    int queueItems[TAMANHOFILA];
+} Fila; // será que necessita dessas variaveis head e tail na encadeada??
+
+
+
+
+int main(void){
+ 
+
+
+    return 0;
+}
