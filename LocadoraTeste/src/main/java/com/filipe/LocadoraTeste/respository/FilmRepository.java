66db76fa9f75fbd35463f73a377d02ab12d51079
Filipe Samuel <felipepires.p125@gmail.com>
2024-10-02 21:27:52 -0300

    initial commit

--- LocadoraTeste/src/main/java/com/filipe/LocadoraTeste/respository/FilmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.filipe.LocadoraTeste.respository;
-
-import com.filipe.LocadoraTeste.entity.Film;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FilmRepository extends JpaRepository<Film,Long> {
-}
--- LocadoraTeste/src/main/java/com/filipe/LocadoraTeste/respository/RegistryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.filipe.LocadoraTeste.respository;
-
-import com.filipe.LocadoraTeste.entity.Registry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistryRepository extends JpaRepository<Registry, Long> {
-}
