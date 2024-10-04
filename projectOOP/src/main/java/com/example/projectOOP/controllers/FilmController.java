87db8a6db5fad8eb647aa27079b74d8df10db9d0
Filipe Samuel <felipepires.p125@gmail.com>
2024-10-04 19:16:18 -0300

    feat: method "findById" add

--- projectOOP/src/main/java/com/example/projectOOP/controllers/FilmController.java
+++ projectOOP/src/main/java/com/example/projectOOP/controllers/FilmController.java
@@ -6,6 +6,7 @@ import com.example.projectOOP.service.FilmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,11 @@ public class FilmController {
         List<Film> filmList = service.findAll();
         return ResponseEntity.ok().body(filmList);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Film> findById(@PathVariable Long id){
+        Film film = service.findById(id);
+        return ResponseEntity.ok().body(film);
+    }
+
 }
--- projectOOP/src/main/java/com/example/projectOOP/service/FilmService.java
+++ projectOOP/src/main/java/com/example/projectOOP/service/FilmService.java
@@ -4,8 +4,10 @@ import com.example.projectOOP.entities.Film;
 import com.example.projectOOP.repositories.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FilmService {
@@ -15,6 +17,11 @@ public class FilmService {
 
     public List<Film> findAll(){
         return repository.findAll();
+    }
+
+    public Film findById(Long id){
+        Optional<Film> film = repository.findById(id);
+        return film.orElseThrow(() -> new ResourceAccessException("Filme não encontrado"));
     }
 
 }
